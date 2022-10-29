681e1e907f821ed1434c4fb2b195cd283af378b4
thiago <nasciutti.thiago@gmail.com>
2022-10-29 13:43:52 -0400

    deploying

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pizza-hunt',
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pizza_hunt_db',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
